35bfce6ff5cfefc643f0cedeea11793de8dfb3e5
lmckenna27 <lmckenna27@brophybroncos.org>
2026-03-10 22:39:15 -0700

    Firestore fix

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -6,9 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
+  apiKey: "AIzaSyD9z72U_9q6EwwLedAijiESyHigsWOYuM8",
   authDomain: "broncobots-scouting-3185e.firebaseapp.com",
   projectId: "broncobots-scouting-3185e",
-  storageBucket: "broncobots-scouting-3185e.appspot.com",
+  storageBucket: "broncobots-scouting-3185e.firebasestorage.app",
+  messagingSenderId: "921887147292",
+  appId: "1:921887147292:web:09f4aa778a33886e71a6d9"
 };
 
 const app = initializeApp(firebaseConfig);
